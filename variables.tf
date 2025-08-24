2fee5a089b60b720e22f6de2410fa515c69ac5ca
Qshar1408 <slider47@yandex.ru>
2025-08-24 11:21:52 +0300

    Check with tflint and checkov and change errors

--- variables.tf
+++ variables.tf
@@ -10,17 +10,6 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "Зона, в которой создать подсеть"
 }
-variable "default_cidr" {
-  type        = list(string)
-  default     = ["10.0.1.0/24"]
-  description = "CIDR-блок для подсети"
-}
-
-variable "vpc_name" {
-  type        = string
-  default     = "develop"
-  description = "VPC network&subnet name"
-}
 
 variable "ssh-authorized-keys" {
   type    = list(string)
